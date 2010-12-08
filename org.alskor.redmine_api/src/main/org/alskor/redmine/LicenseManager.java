2fe3d64cd738655dff504de5a5ee640604d65771
Alexey Skor <alskor@gmail.com>
2010-12-08 21:25:12 +0000

    changing src code structure to be Maven-compatible, adding some basic support for Licensing in Redmine API (not finished yet). adding javadoc generation to Redmine API.

---
+++ org.alskor.redmine_api/src/main/org/alskor/redmine/LicenseManager.java
@@ -0,0 +1,37 @@
+package org.alskor.redmine;
+
+import java.io.IOException;
+import java.util.Calendar;
+
+public class LicenseManager {
+
+	public static Calendar checkLicense() {
+
+		try {
+			String licenseText = MyIOUtils
+					.getResourceAsString("redmineapi.license");
+			return checkLicense(licenseText);
+		} catch (IOException e) {
+			throw new RuntimeException("Problem loading redmine API license: "
+					+ e);
+		}
+	}
+
+	private static Calendar checkLicense(String licenseText) {
+		String validTillStr = licenseText.substring(5);
+		// System.out.println(validTillStr);
+		Calendar validTill = null;
+		try {
+			long validTillMillis = Long.parseLong(validTillStr);
+			validTill = Calendar.getInstance();
+			validTill.setTimeInMillis(validTillMillis);
+			Calendar now = Calendar.getInstance();
+			if (now.after(validTill)) {
+				throw new RuntimeException("Redmine API license expired. License used: " + validTillStr);
+			}
+		} catch (NumberFormatException e) {
+			throw new RuntimeException("Redmine API license is invalid. License used: " + validTillStr);
+		}
+		return validTill;
+	}
+}
