23b774e2b35e2c6df6be78fb0e04bb831f4331a9
Alexey Skor <alskor@gmail.com>
2010-12-08 00:32:48 +0000

    moving code from the old project location.

---
+++ org.alskor.redmine_api.tests/src/org/alskor/redmine/ProjectsTest.java
@@ -0,0 +1,60 @@
+package org.alskor.redmine;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.Iterator;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ProjectsTest {
+
+	private List<Project> projectsList;
+
+	@Before
+	// Is executed before each test method
+	public void setup() throws Exception {
+		String text = MyIOUtils.getResourceAsString("projects.xml");
+		projectsList = RedmineManager.getProjects(text);
+	}
+
+	@Test
+	public void testProjectsNumber() {
+		int expectedProjectsNumber = 6;
+		assertEquals("Checking projects number" , expectedProjectsNumber, projectsList.size());
+	}
+
+	@Test
+	public void testProjects1Info() {
+		Project aceProject = new Project();
+		aceProject.setId(7);
+		aceProject.setIdentifier("ace");
+		aceProject.setName("ace");
+
+		Project projectFromList = findProjectInList(aceProject.getId());
+		assertNotNull("Checking project is loaded", projectFromList);
+		
+		// could use project.equals later when it's implemented in the class
+		assertEquals("Checking the loaded project info" , aceProject.getId(), projectFromList.getId());
+		assertEquals("Checking the loaded project info" , aceProject.getName(), projectFromList.getName());
+		assertEquals("Checking the loaded project info" , aceProject.getIdentifier(), projectFromList.getIdentifier());
+	}
+
+	/*
+	 * @return NULL, if not found in list
+	 */
+	private Project findProjectInList(int projectDbId) {
+		Project result = null;
+		Iterator<Project> it = projectsList.iterator();
+		while (it.hasNext()) {
+			Project project = it.next();
+			if (project.getId().equals(projectDbId)) {
+				result = project;
+				break;
+			}
+		}
+		return result;
+	}
+}
---
+++ org.alskor.redmine_api/src/org/alskor/redmine/Issue.java
@@ -0,0 +1,131 @@
+package org.alskor.redmine;
+
+import java.util.Date;
+
+public class Issue {
+
+	private Integer id;
+	private String subject;
+	private Integer parentId;
+	private Float estimatedHours;
+	private User assignee;
+	private String priorityText;
+	private Integer doneRatio;
+	private Project project;
+	private User author;
+	private Date startDate;
+	private Date dueDate;
+	private Tracker tracker;
+	
+	public Project getProject() {
+		return project;
+	}
+
+	public void setProject(Project project) {
+		this.project = project;
+	}
+
+	public Integer getDoneRatio() {
+		return doneRatio;
+	}
+
+	public void setDoneRatio(Integer doneRatio) {
+		this.doneRatio = doneRatio;
+	}
+
+	public String getPriorityText() {
+		return priorityText;
+	}
+
+	public void setPriorityText(String priority) {
+		this.priorityText = priority;
+	}
+
+	public User getAssignee() {
+		return assignee;
+	}
+
+	public void setAssignee(User assignee) {
+		this.assignee = assignee;
+	}
+
+	public Float getEstimatedHours() {
+		return estimatedHours;
+	}
+
+	public void setEstimatedHours(Float estimatedTime) {
+		this.estimatedHours = estimatedTime;
+	}
+
+	/**
+	 * Parent Issue ID, or NULL for issues without a parent.
+	 * 
+	 * @return NULL, if there's no parent
+	 */
+	public Integer getParentId() {
+		return parentId;
+	}
+
+	public void setParentId(Integer parentId) {
+		this.parentId = parentId;
+	}
+
+	/**
+	 * @return id. can be NULL for Issues not added to Redmine yet
+	 */
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	@Override
+	public String toString() {
+//		return id + "  (parent=" + parentId + ")";
+		
+		return "issue: id=" +id + " assignee:" + ((assignee == null) ? "" : assignee.toString()) + " subject:" + subject;
+	}
+
+	public User getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(User author) {
+		this.author = author;
+	}
+
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+	public Date getDueDate() {
+		return dueDate;
+	}
+
+	public void setDueDate(Date dueDate) {
+		this.dueDate = dueDate;
+	}
+
+	public Tracker getTracker() {
+		return tracker;
+	}
+
+	public void setTracker(Tracker tracker) {
+		this.tracker = tracker;
+	}
+	
+}
