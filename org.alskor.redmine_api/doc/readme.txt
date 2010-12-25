1073556ff8ca30338f75cd1efc044ff16d21e313
Alexey Skor <alskor@gmail.com>
2010-12-25 18:14:06 +0000

    refs #8706 pack redmine API into a ZIP file with all required dependencies and docs.

--- org.alskor.redmine_api/doc/readme.txt
+++ org.alskor.redmine_api/doc/readme.txt
@@ -1,1 +1,6 @@
-Some tutorial here...
+Redmine Java API.
+
+USAGE.
+
+1. Add the redmine api Jar and all jars from "lib" folder to the classpath.
+2. See sample code in the "samples" folder.  
---
+++ org.alskor.redmine_api/doc/samples/TryRedmineAPI.java
@@ -0,0 +1,30 @@
+import java.io.IOException;
+import java.util.List;
+
+import org.alskor.httputils.AuthenticationException;
+import org.alskor.redmine.RedmineManager;
+import org.alskor.redmine.beans.Issue;
+
+public class TryRedmineAPI {
+	private static String redmineHost = "https://www.hostedredmine.com";
+	private static String apiAccessKey = "a3221bfcef5750219bd0a2df69519416dba17fc9";
+	private static String projectKey = "taskconnector-test";
+	private static String queryId = null; // any
+
+	public static void main(String[] args) {
+		RedmineManager mgr = new RedmineManager(redmineHost, apiAccessKey);
+		try {
+			tryGetIssues(mgr);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private static void tryGetIssues(RedmineManager mgr) throws IOException, AuthenticationException {
+		List<Issue> issues = mgr.getIssues(projectKey, queryId);
+		for (Issue issue : issues) {
+			System.out.println(issue.toString());
+		}
+	}
+}
