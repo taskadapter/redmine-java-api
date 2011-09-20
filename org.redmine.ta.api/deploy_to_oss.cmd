a4f0fd5edd9d4c480917435763d568b29f3b585f
Alexey Skor <alskor@gmail.com>
2011-09-20 17:47:53 +0000

    updated "deploy to maven central" script.

--- org.redmine.ta.api/deploy_to_oss.cmd
+++ org.redmine.ta.api/deploy_to_oss.cmd
@@ -1,1 +1,10 @@
-mvn gpg:sign-and-deploy-file -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2 -DrepositoryId=sonatype-nexus-staging -DpomFile=pom.xml -Dfile=target/redmine-java-api-1.5.0.jar -Dfile=target/redmine-java-api-1.5.0-sources.jar
+set DEPLOY_CMD=mvn gpg:sign-and-deploy-file -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2 -DrepositoryId=sonatype-nexus-staging -DpomFile=pom.xml
+set JAR=target/redmine-java-api-1.5.1.jar
+set SRC=target/redmine-java-api-1.5.1-sources.jar
+set JAVADOC=target/redmine-java-api-1.5.1-javadoc.jar
+
+call %DEPLOY_CMD% -Dfile=%JAR%
+call %DEPLOY_CMD% -Dfile=%SRC% -Dclassifier=sources
+call %DEPLOY_CMD% -Dfile=%JAVADOC% -Dclassifier=javadoc
+
+
