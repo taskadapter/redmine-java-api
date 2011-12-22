8218c066891db47176b216e478f3e0fc760ef93f
Alexey Skor <alskor@gmail.com>
2011-12-22 18:07:29 +0000

    updated the deploy sample script to activate the new "deploy-profile" profile (to use GPG signatures).

--- deploy_to_oss.cmd
+++ deploy_to_oss.cmd
@@ -1,10 +1,1 @@
-set DEPLOY_CMD=mvn gpg:sign-and-deploy-file -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2 -DrepositoryId=sonatype-nexus-staging -DpomFile=pom.xml
-set JAR=target/redmine-java-api-1.6.0.jar
-set SRC=target/redmine-java-api-1.6.0-sources.jar
-set JAVADOC=target/redmine-java-api-1.6.0-javadoc.jar
-
-call %DEPLOY_CMD% -Dfile=%JAR%
-call %DEPLOY_CMD% -Dfile=%SRC% -Dclassifier=sources
-call %DEPLOY_CMD% -Dfile=%JAVADOC% -Dclassifier=javadoc
-
-
+mvn clean deploy -P deploy-profile
