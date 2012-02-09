13686f6f037bd249b3be7170964d062373720a09
Alexey Skor <alskor@gmail.com>
2012-02-09 19:06:18 +0000

    refactoring in tests: moved data comparison methods from MyIOUtils to a new class.

--- src/test/java/org/redmine/ta/MyIOUtils.java
+++ src/test/java/org/redmine/ta/MyIOUtils.java
@@ -99,25 +99,4 @@ public class MyIOUtils {
         in.close();
         return buffer.toString();
     }
-
-    public static void testLongDate(int year, int month, int day, int hour, int min, int sec, String timeZone, Date expectedDate) {
-        Calendar c = Calendar.getInstance();
-        c.set(Calendar.YEAR, year);
-        c.set(Calendar.MONTH, month);
-        c.set(Calendar.DAY_OF_MONTH, day);
-        c.set(Calendar.HOUR_OF_DAY, hour);
-        c.set(Calendar.MINUTE, min);
-        c.set(Calendar.SECOND, sec);
-        c.set(Calendar.MILLISECOND, 0);
-        if (timeZone.length() > 0) {
-            c.setTimeZone(TimeZone.getTimeZone(timeZone));
-        }
-        Date actualDate = c.getTime();
-        Assert.assertEquals("Checking date", actualDate, expectedDate);
-    }
-
-    public static void testShortDate(int year, int month, int day, Date expectedDate) {
-        testLongDate(year, month, day, 0, 0, 0, "", expectedDate);
-    }
-
 }
