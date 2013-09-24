effff881051cddcc621eb507d56c8103109bb841
Michal Ciasnocha <ciasnocha.michal@gmail.com>
2013-09-24 10:02:45 +0200

    added field apiKey to the User bean

--- src/main/java/com/taskadapter/redmineapi/internal/RedmineJSONParser.java
+++ src/main/java/com/taskadapter/redmineapi/internal/RedmineJSONParser.java
@@ -542,6 +542,7 @@ public class RedmineJSONParser {
 		result.setMail(JsonInput.getStringOrNull(content, "mail"));
 		result.setCreatedOn(getDateOrNull(content, "created_on"));
 		result.setLastLoginOn(getDateOrNull(content, "last_login_on"));
+                result.setApiKey(JsonInput.getStringOrNull(content, "api_key"));
 		result.setCustomFields(JsonInput.getListOrEmpty(content,
 				"custom_fields", CUSTOM_FIELD_PARSER));
 		final String name = JsonInput.getStringOrNull(content, "name");
