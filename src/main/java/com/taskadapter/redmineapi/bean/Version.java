af0fe92b2f784518d4e7e403c7dc4d6d9c2599a5
Luis Escamilla Martin <luis.escamilla@juntadeandalucia.es>
2014-06-20 17:03:36 +0200

    Support CustomField in Version, Try #2
    At least on Redmine 0.9.0 RC

--- src/main/java/com/taskadapter/redmineapi/bean/Version.java
+++ src/main/java/com/taskadapter/redmineapi/bean/Version.java
@@ -24,6 +24,7 @@ public class Version implements Identifiable {
     private Date dueDate;
     private Date createdOn;
     private Date updatedOn;
+    // At least on Redmine 0.9.0 RC
     private List<CustomField> customFields = new ArrayList<CustomField>();
 
     /**
@@ -123,14 +124,17 @@ public class Version implements Identifiable {
         this.updatedOn = updatedOn;
     }
 
+    // At least on Redmine 0.9.0 RC
     public List<CustomField> getCustomFields() {
         return customFields;
     }
 
+    // At least on Redmine 0.9.0 RC
     public void setCustomFields(List<CustomField> customFields) {
         this.customFields = customFields;
     }
 
+    // At least on Redmine 0.9.0 RC
     public CustomField getCustomFieldById(int customFieldId) {
         for (CustomField customField : customFields) {
             if (customFieldId == customField.getId()) {
--- src/test/java/com/taskadapter/redmineapi/internal/RedmineJSONParserTest.java
+++ src/test/java/com/taskadapter/redmineapi/internal/RedmineJSONParserTest.java
@@ -416,22 +416,16 @@ public class RedmineJSONParserTest {
 
     @Test
     public void testVersionWithCustomField() throws IOException, JSONException {
-	// final String jsonProject = MyIOUtils
-	// .getResourceAsString("redmine_project.json");
-	// final Project project = RedmineJSONParser
-	// .parseProject(RedmineJSONParser.getResponseSingleObject(
-	// jsonProject, "project"));
-
-	String json = MyIOUtils
-		.getResourceAsString("redmine_versionWithCustomField.json");
-	Project version = RedmineJSONParser
-		.parseProject(RedmineJSONParser.getResponseSingleObject(json,
-			"version"));
-
-	Assert.assertEquals(version.getCustomFields().size(), 2);
-	String expectedCustomeFieldValue = "Should have a value";
-	Assert.assertEquals(expectedCustomeFieldValue, version
-		.getCustomFieldById(1).getValue());
-	Assert.assertEquals("", version.getCustomFieldById(6).getValue());
+		String json = MyIOUtils
+			.getResourceAsString("redmine_versionWithCustomField.json");
+		Project version = RedmineJSONParser
+			.parseProject(RedmineJSONParser.getResponseSingleObject(json,
+				"version"));
+	
+		Assert.assertEquals(version.getCustomFields().size(), 2);
+		String expectedCustomeFieldValue = "Should have a value";
+		Assert.assertEquals(expectedCustomeFieldValue, version
+			.getCustomFieldById(1).getValue());
+		Assert.assertEquals("", version.getCustomFieldById(6).getValue());
     }
 }
