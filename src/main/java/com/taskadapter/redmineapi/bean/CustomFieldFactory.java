9de35d8e25e9c568758b6dc0ef8789f7ba9e2536
Alexey Skorokhodov <alskor@gmail.com>
2014-09-07 16:46:32 -0700

    deleted a useless method

--- src/main/java/com/taskadapter/redmineapi/bean/CustomFieldFactory.java
+++ src/main/java/com/taskadapter/redmineapi/bean/CustomFieldFactory.java
@@ -1,10 +1,6 @@
 package com.taskadapter.redmineapi.bean;
 
 public class CustomFieldFactory {
-    public static CustomField create() {
-        CustomField field = new CustomField(null);
-        return field;
-    }
 
     public static CustomField create(Integer id) {
         CustomField field = new CustomField(id);
