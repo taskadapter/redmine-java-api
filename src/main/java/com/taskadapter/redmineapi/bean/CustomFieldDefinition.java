6f20aa79598238ae9791eb7dfac1fbae52387b68
Matthias Bläsing <mblaesing@doppel-helix.eu>
2014-12-28 12:26:11 +0100

    Fix equals/hashCode: Prevent NullPointer Exception in hashCode and use correct comparisson in equals.

--- src/main/java/com/taskadapter/redmineapi/bean/CustomFieldDefinition.java
+++ src/main/java/com/taskadapter/redmineapi/bean/CustomFieldDefinition.java
@@ -146,28 +146,22 @@ public class CustomFieldDefinition {
     public List<Role> getRoles() {
         return roles;
     }
-    
+
     @Override
     public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
 
         CustomFieldDefinition that = (CustomFieldDefinition) o;
 
-        if (id != that.id) {
-            return false;
-        }
+        if (id != null ? !id.equals(that.id) : that.id != null) return false;
 
         return true;
     }
 
     @Override
     public int hashCode() {
-        return id;
+        return id != null ? id.hashCode() : 0;
     }
 
     @Override
