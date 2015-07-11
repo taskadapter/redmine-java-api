6aca756fbc6730174aa4e1ff398e58383be5c897
Alexey Skorokhodov <alskor@gmail.com>
2015-07-11 10:57:48 -0700

    #207 add create(id) method to TrackerFactory class

--- src/main/java/com/taskadapter/redmineapi/bean/TrackerFactory.java
+++ src/main/java/com/taskadapter/redmineapi/bean/TrackerFactory.java
@@ -3,6 +3,13 @@ package com.taskadapter.redmineapi.bean;
 public class TrackerFactory {
     /**
      * @param id database Id
+     */
+    public static Tracker create(int id) {
+        return new Tracker(id);
+    }
+
+    /**
+     * @param id database Id
      * @param name tracker name
      */
     public static Tracker create(int id, String name) {
