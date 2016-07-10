15e54aca49272c8f40b954afb1d60f78cf33ccec
Alex Skorokhodov <alskor@gmail.com>
2016-07-09 20:04:47 -0700

    #213 added some javadoc for Assignee.

--- src/main/java/com/taskadapter/redmineapi/bean/Assignee.java
+++ src/main/java/com/taskadapter/redmineapi/bean/Assignee.java
@@ -3,7 +3,10 @@ package com.taskadapter.redmineapi.bean;
 
 /**
  * Assignee is a named representation of an assignee.
- * 
+ *
+ * Redmine can be configured to allow group assignments for issues:
+ * Configuration option: Settings -> Issue Tracking -> Allow issue assignment to groups
+ *
  * <p>An assignee is can be a user or a group. For parsing a GenericAssignee
  * exists, that only holds a name and id (redmine only returns these infos
  * when querying an issue.</p>
